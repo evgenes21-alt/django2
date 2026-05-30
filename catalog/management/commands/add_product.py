67e04f16e414d65b3544c4a693c938412aa50102
Суспицын Евгений <evgen.es21@gmail.com>
2026-05-30 22:53:02 +0400

    fix

---
+++ catalog/management/commands/add_product.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from catalog.models import Product,Category
+
+
+class Command(BaseCommand):
+    help = 'Добавление тестовых продуктов'
+
+    def handle(self, *args, **kwargs):
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        category, _ = Category.objects.get_or_create(name="кондиционеры")
+
+        products = [
+            { 'name': 'Настенный кондиционер Daichi AIR35AVQ1R / AIR35FV1R серии Air', 'price': 37790.00,'category':category},
+            { 'name': 'aeronik', 'price': 31300.00,'category':category}
+        ]
+
+        for prod in products:
+            product, created = Product.objects.get_or_create(**prod)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Successfully added book: {product.name}'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Book already exists: {product.name}'))
---
+++ catalog/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="наименование",
+        help_text="Введите наименование категории",
+    )
+    discription = models.TextField(blank=True, verbose_name="описание")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Наименование",
+        help_text="Введите наименование товара",
+    )
+    description = models.TextField(blank=True, verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="products/", blank=True, null=True, verbose_name="Изображение"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        blank=True,
+        null=True,
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Цена за покупку"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["-created_at"]  # сортировка по дате создания (новые сверху)
+
+    def __str__(self):
+        return self.name
