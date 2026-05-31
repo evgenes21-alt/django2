fd8db215a7471b29fefd65e3184a8f89d58dd1a3
Суспицын Евгений <evgen.es21@gmail.com>
2026-05-31 21:37:39 +0400

    fix 2

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from catalog.views import home
@@ -23,4 +24,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("catalog/", include("catalog.urls")),
     path("", home, name="home"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
