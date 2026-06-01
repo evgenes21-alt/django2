c2c38fbf1cd4795042e64a3d7bceaf172373da12
Суспицын Евгений <evgen.es21@gmail.com>
2026-06-01 21:58:25 +0400

    fix 3

--- config/urls.py
+++ config/urls.py
@@ -25,3 +25,5 @@ urlpatterns = [
     path("catalog/", include("catalog.urls")),
     path("", home, name="home"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
